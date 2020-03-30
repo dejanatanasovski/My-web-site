77fbfbc26c6062e24f9a5d49a213028dab5c92a3
Dejan Atanasovski <atanasovski_dejan@yahoo.com>
2020-03-30 14:31:29 +0100

    Updates

---
+++ precache-manifest.8ee2d67ca3d76019f9a346b982fc3d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5637f9ec4202af40962d3d7db259e5eb",
+    "url": "/My-web-site/index.html"
+  },
+  {
+    "revision": "cb890d8928b8b89141aa",
+    "url": "/My-web-site/static/css/main.d30d9d4d.chunk.css"
+  },
+  {
+    "revision": "181afd93b2fede8cd724",
+    "url": "/My-web-site/static/js/2.f36ce883.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-web-site/static/js/2.f36ce883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb890d8928b8b89141aa",
+    "url": "/My-web-site/static/js/main.1a9a9267.chunk.js"
+  },
+  {
+    "revision": "e6f4784179cfa68ed241",
+    "url": "/My-web-site/static/js/runtime-main.8e53355b.js"
+  }
+]);
